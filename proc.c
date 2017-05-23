7da7175a8ac9ada573b4ed2d922ac328a9b7f17d
Lagogo25 <lagogo@gmail.com>
2017-05-23 21:08:52 +0300

    create and delete swap file

--- proc.c
+++ proc.c
@@ -157,8 +157,8 @@ fork(void)
   safestrcpy(np->name, proc->name, sizeof(proc->name));
  
   pid = np->pid;
-  //  if (proc != initproc)
-    //    createSwapFile(np); // creates a swap file for proccess so he can use pages
+  if (proc != initproc)
+    createSwapFile(np); // creates a swap file for proccess so he can use pages
     // lock to force the compiler to emit the np->state write last.
   acquire(&ptable.lock);
   np->state = RUNNABLE;
